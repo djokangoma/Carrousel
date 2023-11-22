c00d5253c44c6bf6ee311f6258a4988dc102ec9d
djo kangoma <djokangoma@gmail.com>
2023-11-22 02:23:54 +0100

    add modification

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ buttons.forEach((button)=>{
          const slideActive = document.querySelector(".active");
 
          newIndex = calNextslide + [...slides].indexOf(slideActive); 
-         
+
          if(newIndex < 0) newIndex = [... slides].length-1;
          if(newIndex >= [...slides].length) newIndex=0;
          slides[newIndex].classList.add("active");
 
          slideActive.classList.remove("active")
-         console.log(newIndex);
+        
     })
 })
