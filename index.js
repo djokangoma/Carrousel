d64c6891b703e5153200341a9c5a6f5d1dc029ca
djo kangoma <djokangoma@gmail.com>
2023-11-22 02:14:13 +0100

    gestion de previeuw and next

---
+++ index.js
@@ -0,0 +1,19 @@
+const buttons=document.querySelectorAll(".btn");
+const slides=document.querySelectorAll(".slide");
+
+
+buttons.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+         const calNextslide = e.target.id==="next" ? 1 : -1;
+         const slideActive = document.querySelector(".active");
+
+         newIndex = calNextslide + [...slides].indexOf(slideActive); 
+         
+         if(newIndex < 0) newIndex = [... slides].length-1;
+         if(newIndex >= [...slides].length) newIndex=0;
+         slides[newIndex].classList.add("active");
+
+         slideActive.classList.remove("active")
+         console.log(newIndex);
+    })
+})
